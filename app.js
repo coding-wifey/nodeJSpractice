2ff9c70716c7438600fe98f228f191757c773f2e
Kate <kateyamson@gmail.com>
2018-07-06 23:05:17 +0800

    partial

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
-const fs = require('fs');
-const os = require('os');
+const fs    = require('fs');
+const os    = require('os');
+const notes = require('./notes.js');
 
-let user = os.userInfo();
+let res     = notes.addNote();
+console.log(res);
+/*let user = os.userInfo();
 
-
-fs.appendFile('greetings.txt', `Hello ${user.username}!` , function (err) {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} .` , function (err) {
   if (err) {
    console.log('Unable to write the file');
   }
 });
+*/
