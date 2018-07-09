73eb8a102fb66fe658568912a0851da832860ebb
Kate <kateyamson@gmail.com>
2018-07-09 14:50:16 +0800

    learning 3rd party app loadash

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ console.log('Starting app.js');
 
 const fs    = require('fs');
 const os    = require('os');
+const _     = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+let filteredArray = _.uniq(['Kate', 1, 'Kate', 2, 3, 4, 4]);
+console.log(filteredArray);
+
+// console.log('Result:', notes.add(9, -2));
 
 
 // let res     = notes.addNote();
