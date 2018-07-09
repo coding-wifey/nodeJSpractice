c9277ee3e5ca2e1c9032f1f16d251bf251a67e95
Kate <kateyamson@gmail.com>
2018-07-09 14:06:13 +0800

    notes.app

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const fs    = require('fs');
 const os    = require('os');
 const notes = require('./notes.js');
 
-let res     = notes.addNote();
-console.log(res);
+console.log('Result:', notes.add(9, -2));
+
+
+// let res     = notes.addNote();
+// console.log(res);  
 /*let user = os.userInfo();
 
 fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} .` , function (err) {
