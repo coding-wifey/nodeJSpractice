4d3ffd8e02231615b588fd92a02edd894260df69
Kate <kateyamson@gmail.com>
2018-07-09 17:03:21 +0800

    yargs exercise

--- app.js
+++ app.js
@@ -2,5 +2,24 @@ console.log('Starting app.js');
 
 const fs    = require('fs');
 const _     = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv  = yargs.argv;
+let command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if(command ==='add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note'); 
+} else {
+  console.log('Command not recognized');
+}
