56af58c3649d6a1e3ee62906b25ab6f5afb9bae3
Kate <kateyamson@gmail.com>
2018-07-10 14:20:56 +0800

    adding notes and duplicate code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv  = yargs.argv;
-let command = argv_[0];
+let command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
 
